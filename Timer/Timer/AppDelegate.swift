098b540d3a5448bbf2e2da82aac50851c4b489ff
Bernhard Waidacher <waidacher@denovo.at>
2017-09-09 16:29:33 +0200

    initial commit

---
+++ Timer/Timer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Timer
+//
+//  Created by Bernhard Waidacher on 09.09.17.
+//  Copyright © 2017 Bernhard Waidacher. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
